85a88662494e86b4179c2f92275de1ab2cbba3eb
cheloghm <cheloghm@gmail.com>
2018-10-12 12:11:17 +0100

    initial commit

---
+++ WebSession2/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSession2.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NewMovies()
+        {
+            return View();
+        }
+
+        public ActionResult RandomMovies()
+        {
+            return View();
+        }
+    }
+}
